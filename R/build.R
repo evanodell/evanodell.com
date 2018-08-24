ce181c841846244c60a7d98e7c672ef6b4a15cfc
evan <evanodell91@gmail.com>
2018-08-24 15:19:39 +0100

    build.R

---
+++ R/build.R
@@ -0,0 +1,5 @@
+# Need something to compile latex here to produce a pdf of my CV while building the site
+
+# use system2 command here
+
+# Possibly also combine multiple markdown outputs into a single page using bookdown or something? https://stackoverflow.com/questions/25824795/how-to-combine-two-rmarkdown-rmd-files-into-a-single-output
